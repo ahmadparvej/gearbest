397b061e2efdd051a48da3288431cb691d6e41ce
ahmadparvej <ahmadparvej58@gmail.com>
2022-06-17 09:41:59 +0530

    sorting added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Card } from './../components/Card';
-import { Grid, Stack, Text } from '@chakra-ui/react';
+import { Grid, Stack, Text, Heading, Tag, Flex } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 
 export const Products = ({list}) => {
-    
+  const [items, setItems] = useState(list)
+  const [type, setType] = useState("inc");
+
+  const handlePriceTags = (prc)=>{
+    const newItems = items.filter((item)=>item.price>=prc)
+    console.log(newItems);
+    setItems(newItems)
+  }
   return (
       <>
-      <Stack w="20%"><Text>Filters :</Text></Stack>
-      <Grid w="80%" templateColumns="repeat(3,1fr)" gap={5}>{list.map((product)=>{
+      <Stack w="15%" gap={5}>
+        <Heading as="h1" size="md">Filters :</Heading>
+        <Stack>
+          <Text fontSize={20}  color="grey" >SORT BY: Price</Text>
+          <Tag p={2} fontWeight="500" onClick={()=>setType("inc")}>LOW - HIGH</Tag>
+          <Tag p={2} fontWeight="500" onClick={()=>setType("dec")}>HIGH - LOW</Tag>
+          <Flex wrap="wrap" gap={2}>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(100)}>$100</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(200)}>$200</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(300)}>$300</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(400)}>$400</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(500)}>$500</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(600)}>$600</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(700)}>$700</Tag>
+            <Tag cursor="pointer" onClick={()=>handlePriceTags(1500)}>$1500</Tag>
+          </Flex>
+        </Stack>
+        <Stack>
+          <Text fontSize={20} color="grey" >SORT BY: Rating</Text>
+          <Button size="sm" fontWeight="500" onClick={()=>setType("inc")}>LOW - HIGH</Button>
+          <Button size="sm" fontWeight="500" onClick={()=>setType("dec")}>HIGH - LOW</Button>
+        </Stack>
+      </Stack>
+      <Grid w="80%" templateColumns="repeat(3,1fr)" gap={5}>{items.sort((a,b)=>{
+        if(type==="inc"){
+          return a.price-b.price;
+        }
+        if(type==="dec"){
+          return b.price-a.price;
+        }
+      }).map((product)=>{
           return <Card product={product}/>
       })}</Grid>
       </>
