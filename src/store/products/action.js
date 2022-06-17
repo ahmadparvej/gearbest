280f18b1aeb632d57ffaa511f3fe70ac9f047887
ahmadparvej <ahmadparvej58@gmail.com>
2022-06-17 07:05:40 +0530

    first commit

--- src/store/products/action.js
+++ src/store/products/action.js
@@ -2,10 +2,9 @@
 import { GET_PRODUCTS_SUCCESS, GET_PRODUCTS_LIST_SUCCESS } from './actionTypes';
 
 export const getProducts = (key)=> (dispatch)=>{
-    fetch(`http://localhost:8080/${key}`)
+    fetch(`https://gearbest-json-api.herokuapp.com/${key}`)
     .then((res)=>res.json())
     .then((data)=>dispatch({type:GET_PRODUCTS_SUCCESS,payload:data}))
 }
-
 
 export const getProductListItems = (payload) =>(dispatch)=>(dispatch({type:GET_PRODUCTS_LIST_SUCCESS,payload}))
